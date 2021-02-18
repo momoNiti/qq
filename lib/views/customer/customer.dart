f8dc049b02c323e97c5381e18522d46d32180fb3
momoNiti <60070041@kmitl.ac.th>
2021-02-18 17:50:58 +0700

    แก้ชื่อไฟล์

--- lib/views/customer/customer.dart
+++ lib/views/customer/customer.dart
@@ -2,4 +2,4 @@ export 'home_tab.dart';
 export 'restaurant_detail_screen.dart';
 export 'setting_customer_tab.dart';
 export 'user_queue_tab.dart';
-export 'app__customer_screen.dart';
+export 'app_customer_screen.dart';
